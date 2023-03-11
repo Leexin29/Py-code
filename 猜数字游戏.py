4e530d129d607f6dd4f67b5a9415772a2565e0aa
LEEXIN-MI\Leexin <leexin29@qq.com>
2023-03-11 19:26:15 +0800

    20230311

--- 猜数字游戏.py
+++ 猜数字游戏.py
@@ -1,11 +1,53 @@
-temp=input("不妨猜一下我心里想的是哪个数字:")
+jls_extract_var = '''temp=input("不妨猜一下我心里想的是哪个数字:")
+
 guess=int(temp)
+
 if guess == 8:
+
     print("恭喜你，猜对了！！")
+
     print("哼，猜对了也没有奖励")
+
 else:
+
     print("猜错了，我心里想的是8")
+
 print("游戏结束喽，不玩了")
 
+'''
 
-input("\n\nPress the enter key to exit.")
+
+
+
+
+def guess_number():
+
+    answer = random.randint(1, 10)
+
+    while True:
+
+        try:
+
+            guess = int(input("不妨猜一下我心里想的是哪个数字（1-10之间）："))
+
+            if guess == answer:
+
+                print("恭喜你，猜对了！")
+
+                break
+
+            else:
+
+                print("猜错了，再试一次吧！")
+
+        except ValueError:
+
+            print("请输入数字！")
+
+
+    print("游戏结束，再见！")
+
+
+if __name__ == '__main__':
+
+    guess_number()
